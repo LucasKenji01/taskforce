0496e92de66dad015f126175343671ad251167da
Lucas Kenji <rm94233@fiap.com.br>
2023-10-23 13:07:09 -0300

    internacionalização

--- src/main/java/br/com/fiap/taskforce/TaskforceApplication.java
+++ src/main/java/br/com/fiap/taskforce/TaskforceApplication.java
@@ -2,12 +2,26 @@ package br.com.fiap.taskforce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @SpringBootApplication
-public class TaskforceApplication {
+public class TaskforceApplication implements WebMvcConfigurer {
+
+	private LocaleChangeInterceptor interceptor;
+
+	public TaskforceApplication(LocaleChangeInterceptor interceptor) {
+		this.interceptor = interceptor;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskforceApplication.class, args);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor);
 	}
 
 }
---
+++ src/main/java/br/com/fiap/taskforce/config/LocaleConfig.java
@@ -0,0 +1,35 @@
+package br.com.fiap.taskforce.config;
+
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class LocaleConfig {
+
+  @Bean
+  public MessageSource messageSource() {
+    var messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasename("lang/messages");
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+  }
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    var resolver = new CookieLocaleResolver();
+    return resolver;
+  }
+
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    var interceptor = new LocaleChangeInterceptor();
+    interceptor.setParamName("lang");
+    return interceptor;
+  }
+
+}
